9f1e80d9fed53bc0939b868acc506246ffce9097
moliroliro <rm561290@fiap.com.br>
2025-05-29 10:01:03 -0300

    Adicionando versão inicial do programa

---
+++ Registro_de_Desastres/25.1.GS.Bruno Maurus e Murilo Alencar.py
@@ -0,0 +1,63 @@
+##Bruno Pereira Maurus RM562503
+##Murilo Alencar Pontes RM561290
+
+tipos_desastres = []
+paises = []
+cidades = []
+bairros = []
+ruas = []
+total_afetados = []
+criancas = []
+adultos = []
+idosos = []
+mobilidade_reduzida = []
+feridos = []
+
+num_desastres = int(input("Insira a quantidade de desastres: "))
+
+for i in range(num_desastres):
+    print(f" Desastre {i + 1} ")
+
+    tipos_desastres.append(input("Tipo do desastre: "))
+    paises.append(input("País: "))
+    cidades.append(input("Cidade: "))
+    bairros.append(input("Bairro: "))
+    ruas.append(input("Rua: "))
+
+    while True:
+        total = int(input("Total de pessoas que foram afetadas: "))
+
+        crianca = int(input("Número de crianças: "))
+        adulto = int(input("Número de adultos: "))
+        idoso = int(input("Número de idosos: "))
+        mobilidade = int(input("Número de pessoas com mobilidade reduzida: "))
+        ferido = int(input("Número de feridos: "))
+
+        if crianca + adulto + idoso + mobilidade + ferido == total:
+            criancas.append(crianca)
+            adultos.append(adulto)
+            idosos.append(idoso)
+            mobilidade_reduzida.append(mobilidade)
+            feridos.append(ferido)
+            total_afetados.append(total)
+            break
+        else:
+            print("A quantidade não corresponde ao total, faça novamente")
+
+total_desastre = len(tipos_desastres)
+total_afetados_geral = sum(total_afetados)
+soma_crianca = sum(criancas)
+soma_adulto = sum(adultos)
+soma_idoso = sum(idosos)
+soma_mobilidade = sum(mobilidade_reduzida)
+soma_ferido = sum(feridos)
+
+tipos = [soma_crianca, soma_adulto, soma_idoso, soma_mobilidade, soma_ferido]
+nomes_categorias = ["Crianças", "Adultos", "Idosos", "Mobilidade reduzida", "Feridos"]
+
+indice_mais_afetado = tipos.index(max(tipos))
+categoria_mais_afetada = nomes_categorias[indice_mais_afetado]
+
+mais_grave = total_afetados.index(max(total_afetados))
+local_mais_afetado = f"{ruas[mais_grave]}, {bairros[mais_grave]}, {cidades[mais_grave]}, {paises[mais_grave]}"
+tipo_mais_grave = tipos_desastres[mais_grave]
